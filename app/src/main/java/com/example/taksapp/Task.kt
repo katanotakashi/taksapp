a0aecb00f6899bc2825b7c740eed4e903798afd3
katanotakashi <katanotakashi99@gmail.com>
2019-07-15 16:59:03 +0900

    課題の調整前

---
+++ app/src/main/java/com/example/taksapp/Task.kt
@@ -0,0 +1,16 @@
+package com.example.taksapp
+
+import java.io.Serializable
+import java.util.Date
+import io.realm.RealmObject
+import io.realm.annotations.PrimaryKey
+
+open class Task : RealmObject(), Serializable {
+    var title: String = ""
+    var contents: String = ""
+    var category: String = ""
+    var date: Date = Date()
+
+    @PrimaryKey
+    var id: Int = 0
+}
